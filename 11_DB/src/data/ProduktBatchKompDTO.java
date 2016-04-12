6f9948730a0806afacd2d7706de6f13bd3a5c7cc
Sofie Larsen <sofie_p_larsen@hotmail.com>
2016-04-12 10:00:07 +0200

    Ændringer i ProduktBatchKomponent

--- 11_DB/src/data/ProduktBatchKompDTO.java
+++ 11_DB/src/data/ProduktBatchKompDTO.java
@@ -1,34 +1,61 @@
 package data;
 
-public class ProduktBatchKompDTO 
-{
-	int pbId; 	  // produktbatchets id
-	int rbId;        // i omraadet 1-99999999
+public class ProduktBatchKompDTO {
+	int pbID;
+	int rbID;
 	double tara;
 	double netto;
-	int oprId;					// operatoer-nummer
+	int oprID;
 
-	
-	public ProduktBatchKompDTO(int pbId, int rbId, double tara, double netto, int oprId)
-	{
-		this.pbId = pbId;
-		this.rbId = rbId;
+	public ProduktBatchKompDTO(int pbId, int rbId, double tara, double netto, int oprId){
+		this.pbID = pbId;
+		this.rbID = rbId;
 		this.tara = tara;
 		this.netto = netto;
-		this.oprId = oprId;
+		this.oprID = oprId;
 	}
 	
-	public int getPbId() { return pbId; }
-	public void setPbId(int pbId) { this.pbId = pbId; }
-	public int getRbId() { return rbId; }
-	public void setRbId(int rbId) { this.rbId = rbId; }
-	public double getTara() { return tara; }
-	public void setTara(double tara) { this.tara = tara; }
-	public double getNetto() { return netto; }
-	public void setNetto(double netto) { this.netto = netto; }
-	public int getOprId() { return oprId; }
-	public void setOprId(int oprId) { this.oprId = oprId; }
+	public int getPbId() {
+		return pbID; 
+		}
+	
+	public void setPbId(int pbId) {
+		this.pbID = pbId; 
+		}
+	
+	public int getRbId() {
+		return rbID; 
+		}
+	
+	public void setRbId(int rbId) {
+		this.rbID = rbId; 
+		}
+	
+	public double getTara() {
+		return tara; 
+		}
+	
+	public void setTara(double tara) {
+		this.tara = tara; 
+		}
+	
+	public double getNetto() {
+		return netto; 
+		}
+	
+	public void setNetto(double netto) {
+		this.netto = netto; 
+		}
+	
+	public int getOprId() {
+		return oprID; 
+		}
+	
+	public void setOprId(int oprId) {
+		this.oprID = oprId; 
+		}
+	
 	public String toString() { 
-		return pbId + "\t" + rbId +"\t" + tara +"\t" + netto + "\t" + oprId ; 
+		return pbID + "\t" + rbID +"\t" + tara +"\t" + netto + "\t" + oprID ; 
 	}
 }

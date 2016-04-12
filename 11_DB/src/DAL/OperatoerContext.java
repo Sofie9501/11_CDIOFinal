1d2f4df303ceaf82b0d0723a14958e527c44028c
Morten Due Christiansen <mdue84@gmail.com>
2016-04-12 10:11:45 +0200

    OperatoerContext tilføjet

---
+++ 11_DB/src/DAL/OperatoerContext.java
@@ -0,0 +1,35 @@
+package DAL;
+
+import java.awt.List;
+
+import DTO.OperatoerDTO;
+import interfaces.DALException;
+import interfaces.OperatoerDAO;
+
+public class OperatoerContext implements OperatoerDAO{
+
+	@Override
+	public OperatoerDTO getOperatoer(int oprId) throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List getOperatoerList() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createOperatoer(OperatoerDTO opr) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateOperatoer(OperatoerDTO opr) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

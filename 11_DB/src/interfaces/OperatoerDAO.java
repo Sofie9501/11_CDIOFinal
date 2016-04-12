09007f03bfa17dab40e82e241deedecd3be01438
Sofie Larsen <sofie_p_larsen@hotmail.com>
2016-04-12 09:53:03 +0200

    OperatoerDTO færdig

--- 11_DB/src/interfaces/OperatoerDAO.java
+++ 11_DB/src/interfaces/OperatoerDAO.java
@@ -1,5 +1,12 @@
 package interfaces;
 
+import java.awt.List;
+
+import data.OperatoerDTO;
+
 public interface OperatoerDAO {
-	
+	OperatoerDTO getOperatoer(int oprId) throws DALException;
+	List<OperatoerDTO> getOperatoerList() throws DALException;
+	void createOperatoer(OperatoerDTO opr) throws DALException;
+	void updateOperatoer(OperatoerDTO opr) throws DALException;
 }

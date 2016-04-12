f495bfb0b24c84903deba7b5aa03b10d11d38ac3
Casper <kiwicasper@hotmail.com>
2016-04-12 11:31:12 +0200

    Forsøg med ReceptContext

---
+++ 11_DB/src/DAL/ReceptContext.java
@@ -0,0 +1,68 @@
+package DAL;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import DTO.ReceptDTO;
+import interfaces.DALException;
+import interfaces.ReceptDAO;
+
+public class ReceptContext implements ReceptDAO{
+	
+	Connector c = new Connector();
+
+	@Override
+	public ReceptDTO getRecept(int receptID) throws DALException {
+		String query = "select recept_navn from recept_administration where recept_id ="+ 
+						receptID+ " group by recept_navn";
+		ResultSet result = c.doQuery(query);
+		
+		if(result == null){
+			throw new DALException("Invalid ID");
+		}
+		
+		// Convert to Data Transfer Object
+		ReceptDTO recept = null;
+		try {
+			// is there a next row
+			if(result.next()){
+				recept = new ReceptDTO(receptID, result.getString(1));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		// return operator.
+		return recept;
+	}
+
+	@Override
+	public List<ReceptDTO> getReceptList() throws DALException {
+		String query = "select recept_navn, recept_id from recept_administration group by recept_navn";
+		ResultSet result = c.doQuery(query);
+		if(result == null){
+			throw new DALException("No recepts");
+		}
+		List<ReceptDTO> list = null;
+		try{
+			while(result.next()){
+				list.add(new ReceptDTO())
+			}
+				
+		}
+	}
+
+	@Override
+	public void createRecept(ReceptDTO recept) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateRecept(ReceptDTO recept) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}

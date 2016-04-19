6b88be5ff7e48723e306ab45d7f535fdc975a343
Sofie Larsen <sofie_p_larsen@hotmail.com>
2016-04-19 09:21:42 +0200

    Recept

---
+++ 11_DB/src/DTO/ReceptDTO.java
@@ -0,0 +1,14 @@
+package DTO;
+
+public class ReceptDTO {
+	private int receptID;
+	private String receptNavn;
+
+	public ReceptDTO(int receptID, String receptNavn){
+		this.receptID = receptID;
+		this.receptNavn = receptNavn;
+	}
+	
+	
+
+}

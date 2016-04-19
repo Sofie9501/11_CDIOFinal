4a4249ee25726a729dd6f08d5984c098629fcb18
Sofie Larsen <sofie_p_larsen@hotmail.com>
2016-04-19 10:12:13 +0200

    Oprettet RaavareContext

---
+++ 11_DB/src/DAL/RaavareContext.java
@@ -0,0 +1,35 @@
+package DAL;
+
+import java.util.List;
+
+import DTO.RaavareDTO;
+import interfaces.DALException;
+import interfaces.RaavareDAO;
+
+public class RaavareContext implements RaavareDAO{
+
+	@Override
+	public RaavareDTO getRaavare(int raavareId) throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<RaavareDTO> getRaavareList() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createRaavare(RaavareDTO raavare) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateRaavare(RaavareDTO raavare) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

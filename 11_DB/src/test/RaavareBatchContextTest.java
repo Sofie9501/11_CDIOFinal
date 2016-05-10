98bf9962c1bfd1ed5153aa8194894efb9a01fbf2
Sofie Larsen <sofie_p_larsen@hotmail.com>
2016-05-10 10:48:34 +0200

    RaavareBatchContext virker

--- 11_DB/src/test/RaavareBatchContextTest.java
+++ 11_DB/src/test/RaavareBatchContextTest.java
@@ -16,48 +16,43 @@ public class RaavareBatchContextTest {
 
 	@Test
 	public void test() {
-		// TODO Auto-generated method stub
 		RaavareBatchDAO rbd = new RaavareBatchContext();
 
-		System.out.println("\nget raavarebatch list test");
+		System.out.println("\nGet Raavarebatch List:");
 		try {
 			List<RaavareBatchDTO> list = rbd.getRaavareBatchList();
 			for (RaavareBatchDTO dto: list){
 				System.out.println(dto);
 			}
 		} catch (DALException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		System.out.println("\ncreate raavarebatch test");
-		RaavareBatchDTO raavare = new RaavareBatchDTO(103,"pest" , 1500,5 );
+		System.out.println("\nCreate raavarebatch...");
+		RaavareBatchDTO raavare = new RaavareBatchDTO(103, "Pest" , 1500,5 );
 
 		try {
 			rbd.createRaavareBatch(raavare);
 
 		} catch (DALException e) {
-			// TODO Auto-generated catch block
 			System.out.println(e.getMessage());
 		}
-		raavare = new RaavareBatchDTO(10291,"mere pest" , 2050,5 );
+		raavare = new RaavareBatchDTO(10291,"Mere pest" , 2050,5 );
 
 		try {
 			rbd.createRaavareBatch(raavare);
 
 		} catch (DALException e) {
-			// TODO Auto-generated catch block
 			System.out.println(e.getMessage());
 		}
 		
-		System.out.println("\nget raavarebatch list for raavare id");
+		System.out.println("\nGet raavarebatch list for raavare ID: ");
 		try {
 			
 			for(RaavareBatchDTO dto: rbd.getRaavareBatchList(5)){
 				System.out.println(dto);
 			}
 		} catch (DALException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 

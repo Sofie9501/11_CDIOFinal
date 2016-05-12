74011b2d5f5a86ebbbf19ffcd1fb95e036a5529a
Cecilie <cille112@yahoo.dk>
2016-05-12 16:34:10 +0200

    Status bliver nu opdateret

--- 11_DB/src/test/AfvejningTest.java
+++ 11_DB/src/test/AfvejningTest.java
@@ -11,27 +11,36 @@ import interfaces.DALException;
 
 public class AfvejningTest {
 
+	//Før denne test skal køres, skal ProduktBatchAdminContextTest køres først!!!
 	@Test
 	public void test() {
 		AfvejningDAO dao = new AfvejningContext();
+
 		
+		try {
+			dao.createProduktbatchKomponent(new ProduktBatchKomponentDTO(4,2,1,50,2));
+		} catch (DALException e) {
+			e.printStackTrace();
+		}
+
 		try {
 			System.out.println(dao.getAfvejning(4));
 		} catch (DALException e) {
 			e.printStackTrace();
 		}
-		
+
 		try {
-			dao.createProduktbatchKomponent(new ProduktBatchKomponentDTO(1,5,1,50,2));
+			dao.createProduktbatchKomponent(new ProduktBatchKomponentDTO(6,5,1,50,2));
 		} catch (DALException e) {
 			e.printStackTrace();
 		}
-		
+
 		try {
-			System.out.println(dao.getAfvejning(4));
+			System.out.println(dao.getAfvejning(6));
 		} catch (DALException e) {
 			e.printStackTrace();
 		}
 	}
-
 }
+
+

5c6cc64a96932a46a4aa6f2781f7b2f2d71922f6
Sofie Larsen <sofie_p_larsen@hotmail.com>
2016-05-12 13:50:06 +0200

    Kommentarer

--- 11_DB/src/DAL/ReceptKompContext.java
+++ 11_DB/src/DAL/ReceptKompContext.java
@@ -17,13 +17,16 @@ public class ReceptKompContext implements ReceptKompDAO{
 	public ReceptKompDTO getReceptKomp(int receptId, int raavareId) throws DALException {
 		query = "Select * From recept_administration where recept_id = " + receptId +" and raavare_id = " + raavareId;
 		ResultSet result = c.doQuery(query);
-		
+
+		// if there's no receptkomp with the given IDs, throw exception
 		if(result==null){
 			throw new DALException("No receptkomp found");
 		}
-		
+
+		// create an object of receptkomp
 		ReceptKompDTO reKomp = null;
 		try{
+			// give the data to the empty object above
 			if(result.next()){
 				reKomp = new ReceptKompDTO(result.getInt(2),result.getString(3), result.getInt(4), result.getDouble(5), result.getDouble(6));
 
@@ -34,45 +37,45 @@ public class ReceptKompContext implements ReceptKompDAO{
 		return reKomp;
 	}
 
+	// get a list of receptkomps from a receptID 
 	@Override
 	public List<ReceptKompDTO> getReceptKompList(int receptId) throws DALException {
 		query = "select * from recept_administration where recept_id ="+receptId;
 		ResultSet result = c.doQuery(query);
-		
+
 		if (result==null){
 			throw new DALException("No receptkomp found");
 		}
-		
+
 		List<ReceptKompDTO> list =  new ArrayList<ReceptKompDTO>();
 		try {
 			while(result.next()){
 				list.add(new ReceptKompDTO(result.getInt(2),result.getString(3), result.getInt(4), result.getDouble(5), result.getDouble(6)));
 			}
 		} catch (SQLException e) {
-			// TODO: handle exception
 		}
 		return list;
 	}
 
+	// get a list of all the receptkomps 
 	@Override
 	public List<ReceptKompDTO> getReceptKompList() throws DALException {
 		query = "select * from recept_administration"; 
 		ResultSet result = c.doQuery(query);
-		
+
 		if (result==null){
 			throw new DALException("No receptkomp found");
 		}
-		
+
 		List<ReceptKompDTO> list = new ArrayList<ReceptKompDTO>();
 		try {
 			while(result.next()){
 				list.add(new ReceptKompDTO(result.getInt(2), result.getString(3), result.getInt(4), result.getDouble(5), result.getDouble(6)));
 			}
 		} catch (SQLException e) {
-			// TODO: handle exception
 		}
 		return list;
-		
+
 	}
 
 	@Override
@@ -80,7 +83,7 @@ public class ReceptKompContext implements ReceptKompDAO{
 		query = "call opret_receptkomponent(" + receptkomponent.getReceptID() + ", " + receptkomponent.getRaavareID() + 
 				", " + receptkomponent.getNom_netto() + ",  " + receptkomponent.getTolerence() + ")";
 		c.doQuery(query);
-		
+
 	}
 
 }

658eb75aa4db446fb512dd19ebb50d537c4923e9
Morten Due Christiansen <mdue84@gmail.com>
2016-06-04 09:55:22 +0200

    removed db project from repo

--- 11_DB/src/interfaces/AfvejningDAO.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-import DTO.AfvejningDTO;
-import DTO.ProduktBatchKomponentDTO;
-
-public interface AfvejningDAO {
-	public AfvejningDTO getAfvejning(int pb_id) throws DALException;
-	public void createProduktbatchKomponent(ProduktBatchKomponentDTO pbDTO) throws DALException;
-}

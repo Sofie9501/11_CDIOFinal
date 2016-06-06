b567f772e71b74a8273482da7e070341974858ca
Sofie Larsen <sofie_p_larsen@hotmail.com>
2016-06-06 10:30:21 +0200

    RecipeDAO fix

--- G11_CDIOFinal_GWT/src/dk/dtu/cdiofinal/DAO/RecipeDAO.java
+++ G11_CDIOFinal_GWT/src/dk/dtu/cdiofinal/DAO/RecipeDAO.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import dk.dtu.cdiofinal.server.DAL.DALException;
+import dk.dtu.cdiofinal.shared.RecipeComponentDTO;
 import dk.dtu.cdiofinal.shared.RecipeDTO;
 
 public interface RecipeDAO {
 	
 	RecipeDTO getRecipe(int ID) throws DALException;
 	List<RecipeDTO> getRecipeList() throws DALException;
-	void createRecipe(RecipeDTO recipe, ArrayList<ReceptComponentDTO> komp) throws DALException;
+	void createRecipe(RecipeDTO recipe, ArrayList<RecipeComponentDTO> komp) throws DALException;
 	void updateRecipe(RecipeDTO recipe) throws DALException;
 	boolean isActive(RecipeDTO recipe) throws DALException;
 

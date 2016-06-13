15030eaace288bc00649247903073ba3343ba237
Cecilie <cille112@yahoo.dk>
2016-06-13 20:19:32 +0200

    comments

--- G11_CDIOFinal_GWT/src/dk/dtu/cdiofinal/client/serverconnection/ingredientbatch/ClientIngredientBatchImpl.java
+++ G11_CDIOFinal_GWT/src/dk/dtu/cdiofinal/client/serverconnection/ingredientbatch/ClientIngredientBatchImpl.java
@@ -16,34 +16,34 @@ public class ClientIngredientBatchImpl implements IngredientBatchServiceAsync{
 		this.service = GWT.create(IngredientBatchService.class);
 	}
 	
+	//get litd of all ingredient batches
 	@Override
 	public void getIngredientBatches(AsyncCallback<List<IngredientBatchDTO>> callback) {
 		this.service.getIngredientBatches(callback);
-		
 	}
 
+	//get certain ingreidentbatch
 	@Override
 	public void getIngredientBatch(AsyncCallback<IngredientBatchDTO> callback) {
 		this.service.getIngredientBatch(callback);
-		
 	}
 
+	//update ingredientbatch
 	@Override
 	public void updateIngredientBatch(IngredientBatchDTO IngredientBatch, int oldID, AsyncCallback<Boolean> callback) {
 		this.service.updateIngredientBatch(IngredientBatch, oldID, callback);
-		
 	}
 
+	//create ingredientbatch
 	@Override
 	public void createIngredientBatch(IngredientBatchDTO IngredientBatch, AsyncCallback<Boolean> callback) {
 		this.service.createIngredientBatch(IngredientBatch, callback);
-		
 	}
 	
+	//get list of ingredient batches of certain ingredient
 	@Override
 	public void getIngredientBatchesList(int ID, AsyncCallback<List<IngredientBatchDTO>> callback) {
 		this.service.getIngredientBatchesList(ID, callback);
-		
 	}
 
 }
--- G11_CDIOFinal_GWT/src/dk/dtu/cdiofinal/client/serverconnection/recipe/ClientRecipeImpl.java
+++ G11_CDIOFinal_GWT/src/dk/dtu/cdiofinal/client/serverconnection/recipe/ClientRecipeImpl.java
@@ -18,45 +18,45 @@ public class ClientRecipeImpl implements RecipeServiceAsync{
 		this.service = GWT.create(RecipeService.class);
 	}
 
+	//get list of all recipes
 	@Override
 	public void getRecipies(AsyncCallback<List<RecipeDTO>> callback) {
 		this.service.getRecipies(callback);
-		
 	}
 	
+	//get certain recipe
 	@Override
 	public void getRecipe(int ID, AsyncCallback<RecipeDTO> callback) {
 		this.service.getRecipe(ID, callback);
-		
 	}
 	
+	//get list of all component of certain recipe
 	@Override
 	public void getRecipeComponentList (int ID, AsyncCallback<List<RecipeComponentDTO>> callback){
 		this.service.getRecipeComponentList (ID, callback);
 	}
 
-
+	//update recipe
 	@Override
 	public void updateRecipe(RecipeDTO recipe, int oldID, AsyncCallback<Boolean> callback) {
 		this.service.updateRecipe(recipe, oldID, callback);
-		
 	}
 	
+	//update recipecomponent
 	@Override
 	public void updateRecipeComponent(RecipeComponentDTO comp, int oldRecipeID, int oldIngredientID, AsyncCallback<Boolean> callback) {
 		this.service.updateRecipeComponent(comp, oldRecipeID, oldIngredientID, callback);
-		
 	}
 
+	//create recipe
 	@Override
 	public void createRecipe(RecipeDTO recipe, ArrayList<RecipeComponentDTO> list, AsyncCallback<Boolean> callback) {
 		this.service.createRecipe(recipe, list, callback);
 	}
 
+	//create recipecomponent
 	@Override
 	public void createRecipeComponent(RecipeComponentDTO comp, AsyncCallback<Boolean> callback) {
 		this.service.createRecipeComponent(comp, callback);
-		
 	}
-
 }

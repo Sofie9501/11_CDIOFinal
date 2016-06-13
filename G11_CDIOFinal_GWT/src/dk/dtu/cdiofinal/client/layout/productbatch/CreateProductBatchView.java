159dd05c99222761819f288338d70a020b083344
Cecilie <cille112@yahoo.dk>
2016-06-13 19:47:59 +0200

    productbatch opdateret

--- G11_CDIOFinal_GWT/src/dk/dtu/cdiofinal/client/layout/productbatch/CreateProductBatchView.java
+++ G11_CDIOFinal_GWT/src/dk/dtu/cdiofinal/client/layout/productbatch/CreateProductBatchView.java
@@ -26,48 +26,43 @@ import dk.dtu.cdiofinal.shared.ProductBatchDTO;
 import dk.dtu.cdiofinal.shared.RecipeDTO;
 
 public class CreateProductBatchView extends AbstractView {
-	
+
 	final ProdView prod;
 	private ClientProductBatchImpl serviceImpl;
 	private static CreateProductBatchViewUiBinder uiBinder = GWT.create(CreateProductBatchViewUiBinder.class);
-	private ProductBatchDTO batch;
+	private ProductBatchDTO productBatch;
 	private RecipeDTO recipe;
-	
+
 	@UiTemplate("createProductBatchView.ui.xml")
 	interface CreateProductBatchViewUiBinder extends UiBinder<Widget, CreateProductBatchView> {
 	}
-	
-	//Textbox, buttons, heading and modal
-		@UiField
-		TextBox txt_ProdBatchID;
-		@UiField
-		TextBox txt_ReceptID;
-		@UiField
-		Button btn_save;
 
-		@UiField
-		Modal popup;
-		@UiField
-		Button btn_ok;
-		@UiField
-		Heading ok;
+	//Textbox, buttons, heading and modal
+	@UiField TextBox txt_ProdBatchId;
+	@UiField TextBox txt_ReceptId;
+	@UiField Button btn_save;
+	@UiField Modal popup;
+	@UiField Button btn_ok;
+	@UiField Heading head_ok;
 
 	public CreateProductBatchView(ProdView prod) {
 		initWidget(uiBinder.createAndBindUi(this));
 		this.prod=prod;
 		this.serviceImpl = new ClientProductBatchImpl();
-		
+
 		//Add click and key handler to buttons and last textbox
 		btn_save.addClickHandler(new SaveClickHandler());
 		btn_ok.addClickHandler((ClickHandler)new OkClickHandler());
-		txt_ReceptID.addKeyDownHandler(new EnterHandler());
-		
+		txt_ReceptId.addKeyDownHandler(new EnterHandler());
+
 
 	}
+	
+	//checks if the information is valid, return false if not
 	private boolean changeSucces(){
 		String alert = "";
 		boolean succes = true;
-		if(!FieldVerifier.numberValid(Integer.parseInt(txt_ProdBatchID.getText()))){
+		if(!FieldVerifier.numberValid(Integer.parseInt(txt_ProdBatchId.getText()))){
 			alert+="Error - You need to write a valid batch ID \n";
 			succes = false;
 		}
@@ -75,7 +70,7 @@ public class CreateProductBatchView extends AbstractView {
 			alert+="Error - The recipe is not active \n";
 			succes = false;
 		}
-		if(!FieldVerifier.numberValid(Integer.parseInt(txt_ReceptID.getText()))){
+		if(!FieldVerifier.numberValid(Integer.parseInt(txt_ReceptId.getText()))){
 			alert += "Error - ID for Recept is not valid \n";
 			succes = false;
 		}
@@ -86,56 +81,52 @@ public class CreateProductBatchView extends AbstractView {
 	private void saveChanges(){
 		// Checks to see if there is no errors
 		if(changeSucces()){
-			batch = new ProductBatchDTO();
-			batch.setPb_ID(Integer.parseInt(txt_ProdBatchID.getText()));
-			batch.setR_ID(Integer.parseInt(txt_ReceptID.getText()));
-			ok.setText("Your information has been saved");
+			productBatch = new ProductBatchDTO();
+			productBatch.setPb_ID(Integer.parseInt(txt_ProdBatchId.getText()));
+			productBatch.setR_ID(Integer.parseInt(txt_ReceptId.getText()));
+			head_ok.setText("Your information has been saved");
 			//Updates the DB with the new productBatch
-			serviceImpl.createProductBatch(batch, new MyCallback());
-			
-			
+			serviceImpl.createProductBatch(productBatch, new CreateProductBatchCallback());
 		}	
-
 	}
-	
+
+	//creates clientserviceimpl to get recipe with id
 	private void checkRecipeStatus(int Id){
 		ClientRecipeImpl serviceRecipeImpl = new ClientRecipeImpl();
 		serviceRecipeImpl.getRecipe(Id, new DTOCallBack());
 	}
-	
-	//Gets the ingredient with the written ID
-		private class DTOCallBack implements AsyncCallback<RecipeDTO>{
 
-			@Override
-			public void onFailure(Throwable caught) {
-				popup.setTitle("Error");
-				ok.setText("No connection to server");
-				popup.toggle();
-			}
-
-			@Override
-			public void onSuccess(RecipeDTO result) {
-				recipe = result;
-				//call saveChanges when DTO has been returned
-				saveChanges();
-			}
-		}
-	
-	private class SaveClickHandler implements ClickHandler{
+	//Gets the recipe with the written ID
+	private class DTOCallBack implements AsyncCallback<RecipeDTO>{
 
 		@Override
+		public void onFailure(Throwable caught) {
+			popup.setTitle("Error");
+			head_ok.setText("No connection to server");
+			popup.toggle();
+		}
+
+		@Override
+		public void onSuccess(RecipeDTO result) {
+			recipe = result;
+			//call saveChanges when DTO has been returned
+			saveChanges();
+		}
+	}
+
+	//click and enter handlers
+	private class SaveClickHandler implements ClickHandler{
+		@Override
 		public void onClick(ClickEvent event) {
-			checkRecipeStatus(Integer.parseInt(txt_ReceptID.getText()));
+			checkRecipeStatus(Integer.parseInt(txt_ReceptId.getText()));
 			prod.PreviousView();
 		}	
 	}	
 	private class OkClickHandler implements ClickHandler{
-
 		@Override
 		public void onClick(ClickEvent event) {
 			popup.toggle();
 			prod.PreviousView();
-
 		}
 	}
 	private class EnterHandler implements KeyDownHandler {
@@ -143,36 +134,35 @@ public class CreateProductBatchView extends AbstractView {
 		@Override
 		public void onKeyDown(KeyDownEvent event) {
 			if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER){
-				checkRecipeStatus(Integer.parseInt(txt_ReceptID.getText()));
+				checkRecipeStatus(Integer.parseInt(txt_ReceptId.getText()));
 			}		
 		}	
 	}
-	private class MyCallback implements AsyncCallback<Boolean>{
-
+	
+	
+	private class CreateProductBatchCallback implements AsyncCallback<Boolean>{
 		@Override
 		public void onFailure(Throwable caught) {
 			popup.setTitle("Error");
-			ok.setText("No server connection");
+			head_ok.setText("No server connection");
 			popup.toggle();
 		}
 		@Override
 		public void onSuccess(Boolean result) {
 			if(result){
-			popup.toggle();
-			txt_ProdBatchID.setText("");
-			txt_ReceptID.setText("");
+				popup.toggle();
+				txt_ProdBatchId.setText("");
+				txt_ReceptId.setText("");
 			}
 			else{
 				popup.setTitle("Error");
-				ok.setText("An error has occurred, and your information has not been saved.");
+				head_ok.setText("An error has occurred, and your information has not been saved.");
 				popup.toggle();
 			}
 		}		
 	}
 	@Override
 	public void Update() {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
